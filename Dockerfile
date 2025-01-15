5432c7ad2c5e37dabc3b91e26b1a52f75b0dbd21
twoballs-ai <twoballs20@gmail.com>
2025-01-15 20:50:18 +0300

    прод

---
+++ Dockerfile
@@ -0,0 +1,44 @@
+# Use the official Node.js image for building your application
+FROM node:20 AS build
+
+# Set the working directory inside the container
+WORKDIR /app
+
+# Copy package.json and package-lock.json to the working directory
+COPY package*.json ./
+
+# Install dependencies
+RUN npm install
+
+# Copy the rest of your application code
+COPY . .
+
+# Copy the .env file to the working directory
+# COPY .env.local .env
+
+# Build the Next.js application
+RUN npm run build
+
+# Use a smaller Node.js image for serving the application
+FROM node:18-alpine
+
+# Set the working directory inside the container
+WORKDIR /app
+
+# Copy the built application from the previous stage
+COPY --from=build /app/.next ./.next
+COPY --from=build /app/public ./public
+COPY --from=build /app/package.json ./package.json
+# COPY --from=build /app/.env ./.env
+
+# Install only production dependencies
+RUN npm install --only=production
+
+# Set a different port, e.g., 4000
+ENV PORT 5000
+
+# Expose the new port
+EXPOSE 5000
+
+# Command to run the application
+CMD ["npm", "start"]
--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,7 +2,9 @@
 import { AuthProvider } from "../contexts/AuthProvider";
 import "./globals.css";
 import Header from "@/components/Header";
-
+import Script from 'next/script';
+import YandexMetrika from '@/components/metrics/YandexMetrika';
+import { Suspense } from 'react';
 export default function RootLayout({ children }) {
   return (
     <AuthProvider>
@@ -73,6 +75,25 @@ export default function RootLayout({ children }) {
               </div>
             </div>
           </footer>
+          <Script id="metrika-counter" strategy="afterInteractive">
+            {`(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+              (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+ 
+              ym(99538507, "init", {
+                    defer: true,
+                    clickmap:true,
+                    trackLinks:true,
+                    accurateTrackBounce:true,
+                    webvisor:true
+              });`
+            }
+          </Script>
+          <Suspense fallback={<></>}>
+            <YandexMetrika />
+          </Suspense>
         </body>
       </html>
     </AuthProvider>
